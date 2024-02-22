40c8daccb8369ac9717931388a3322c3ab0a9276
Nikolya <kalnitsky.m.i@gmail.com>
2024-02-22 15:12:36 +0200

    change entrypoint.sh

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-if [ -d "/app/vendor" ]; then
+if [ ! -d "/app/vendor" ]; then
+    echo "Running composer install..."
+    composer install
+else
     echo "Executing migrations..."
     php yii migrate --interactive=0
 fi
-
 
 exec "$@"
 
