f3e69c3af9e98efe8809bad7a9e319af021c7129
Nikolya <kalnitsky.m.i@gmail.com>
2024-06-09 22:41:56 +0300

    Change entrypoint.sh

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+# Check if the /app/vendor directory exists
 if [ ! -d "/app/vendor" ]; then
     echo "Running composer install..."
     composer install
 else
     echo "Executing migrations..."
+    # Execute migrations for the main application
     php yii migrate --interactive=0
+
+    # Check if the /app/yii_test file exists
+    if [ ! -f "/app/yii_test" ]; then
+        # Execute migrations for the test application
+        php yii_test migrate --interactive=0
+    fi
 fi
 
+# Execute the command passed to the script
 exec "$@"
-
